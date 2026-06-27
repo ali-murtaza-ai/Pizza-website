b80d257b335e2da633a38eb299d03884602ffad0
ali-murtaza-ai <alimurtaza252268@gmail.com>
2026-06-27 12:11:10 +0500

    added

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
-console.log("Testing")
+console.log("Testing");
 //when you are working with some other person code you need
 // to create a branch 
 // git branch feat/login   for feautre
 // git branch bug fix
+console.log("Testing Successfully");
